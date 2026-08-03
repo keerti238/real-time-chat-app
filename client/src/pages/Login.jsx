7365931505967200cd87a8221b4c02dabc32aa08
Keerti.Bannimatti <01fe24bca238@kletech.ac.in>
2026-08-03 19:45:27 +0530

    Fixed API URLs for production

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,9 +28,9 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        formData
-      );
+  "https://real-time-chat-app-r46v.onrender.com/api/auth/login",
+  formData
+);
 
       localStorage.setItem("token", res.data.token);
       localStorage.setItem(
